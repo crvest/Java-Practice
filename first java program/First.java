a81c291362d650d47662562ee9b92a3668bf475b
crvest <101825580+crvest@users.noreply.github.com>
2023-05-18 15:12:20 -0400

    Delete first java program directory

--- first java program/First.java
+++
@@ -1,7 +0,0 @@
-public class First {
-    public static void main(String[] args) {
-        System.out.println("My name is Chad");
-        System.out.println("I am 39 years old");
-        System.out.println("My hometown is Boston");
-    }
-}
